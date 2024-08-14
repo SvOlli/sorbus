4fc89bf2bc7a3ea3994fed67fceb10387a0c79f3
Sven Oliver Moll <svolli@svolli.de>
2024-08-14 07:26:19 +0200

    src/tools/external-llvm-mos-sdk.sh: fix compile problems
    - some reorganization of the script failed -> fixed
    - also workaround for compile problem in v19.1.0
    
    Signed-off-by: Sven Oliver Moll <svolli@svolli.de>

--- src/tools/external-llvm-mos-sdk.sh
+++ src/tools/external-llvm-mos-sdk.sh
@@ -47,19 +47,20 @@ download()
 
 source()
 {
-   local llvm_archive="${PWD}/llvm-mos-linux-debug.tar.gz"
-   local sdk_archive="${PWD}/${version}.tar.gz"
-   local llvm_dir="llvm-mos-llvm-mos-linux-debug"
-   local sdk_dir="llvm-mos-sdk-$(echo ${version} | cut -c2-)"
-
-   echo "downloading source distribution"
-   setversion
-
    # verify that ninja is available
    type ninja
 
    mkdir -p "${build_dir}"
    cd "${build_dir}"
+
+   echo "downloading source distribution"
+
+   setversion
+
+   local llvm_archive="${PWD}/llvm-mos-linux-debug.tar.gz"
+   local sdk_archive="${PWD}/${version}.tar.gz"
+   local llvm_dir="llvm-mos-llvm-mos-linux-debug"
+   local sdk_dir="llvm-mos-sdk-$(echo ${version} | cut -c2-)"
 
    download \
       https://github.com/llvm-mos/llvm-mos/archive/refs/tags/llvm-mos-linux-debug.tar.gz \
@@ -86,6 +87,11 @@ source()
       echo "unpacking sdk"
       tar xf "${sdk_archive}"
    fi
+
+   # this break compilation with v19.1.0
+   sed -e 's/typedef unsigned size_t;/typedef __SIZE_TYPE__ size_t;/' \
+       -i "${sdk_dir}/mos-platform/common/include/unistd.h"
+
    cmake -S "${sdk_dir}" -B build-llvm-sdk -G Ninja \
       -DCMAKE_INSTALL_PREFIX="${target_dir}" \
       -DCMAKE_PREFIX_PATH="${target_dir}" \
