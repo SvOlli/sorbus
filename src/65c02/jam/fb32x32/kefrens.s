a1a9c42206a81a69da86221f7a0a5a27ca55f5ab
Sven Oliver Moll <svolli@svolli.de>
2025-09-14 13:08:44 +0200

    added missing kefrens
    Signed-off-by: Sven Oliver Moll <svolli@svolli.de>

---
+++ src/65c02/jam/fb32x32/kefrens.s
@@ -0,0 +1,130 @@
+
+.include "fb32x32.inc"
+
+.define DELAY 500
+
+vector = $10
+tmp8   = $12
+frmcnt = $13
+chroma = $14
+luma   = $15
+
+vector1 = $20
+vector2 = $22
+
+tmpbuf      := $0200
+FRAMEBUFFER := $cc00
+
+rtab     = $02cd
+gtab     = $02de
+btab     = $02ef
+
+start:
+   jmp   init
+
+screenupdate:
+
+irqhandler:
+   pha
+   phx
+   phy
+
+   jsr   screenupdate
+   stz   FB32X32_COPY
+
+   lda   TMIMRL         ; acknoledge timer
+   ply
+   plx
+   pla
+   rti
+
+init:
+   lda   #<FRAMEBUFFER
+   sta   vector+0
+   ldx   #>FRAMEBUFFER
+   stx   vector+1
+   ldy   #$00
+   int   FB32X32
+   stz   FB32X32_COLMAP ; default rgbi2222
+
+   lda   #$07
+   sta   chroma
+   stz   luma
+
+   jsr   setupfb
+
+   sei
+   lda   #<irqhandler
+   sta   UVNBI+0
+   lda   #>irqhandler
+   sta   UVNBI+1
+   lda   #<DELAY
+   sta   TMIMRL
+   lda   #>DELAY
+   sta   TMIMRH
+
+.if BARS & 0xFF
+   lda   #<BARS
+   sta   vector+0
+.else
+   stz   vector+0
+.endif
+   lda   #>BARS
+   sta   vector+1
+
+
+   lda   #<(colordataend-colordata)
+   sta   tmp8
+
+@preploop:
+
+   tya
+   and   #$03
+   ora   tmp8
+   tax
+
+   lda   tmp8
+   sec
+   sbc   #$04
+   sta   tmp8
+   bne   @preploop
+
+   cli
+
+   jsr   PRINT
+   .byte 10,"press space to toggle mode"
+   .byte 10,"(CTRL-C to quit) ",0
+
+@mainloop:
+   jsr   CHRIN
+   cmp   #$20
+   bne   :+
+   lda   tmp8
+   eor   #$01
+   sta   tmp8
+:
+   cmp   #$03
+   bne   @mainloop
+
+   stz   TMIMRL
+   stz   TMIMRH
+
+   jmp   ($fffc)
+
+colordata:
+   ; red
+   .byte $40,$80,$c0,$c3
+   ; green
+   .byte $10,$20,$30,$33
+   ; blue
+   .byte $04,$08,$0c,$0f
+   ; yellow (red+green)
+   .byte $50,$a0,$f0,$f3
+   ; purple (red+blue)
+   .byte $44,$88,$cc,$c3
+   ; cyan (green+blue)
+   .byte $14,$28,$3c,$3f
+   ; white
+   .byte $54,$a8,$fc,$ff
+colordataend:
+
