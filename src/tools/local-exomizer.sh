6d4271190f0241cb5fd219198f57ceaaf1ba2762
Sven Oliver Moll <svolli@svolli.de>
2026-07-03 17:47:05 +0200

    src/tools/local-exomizer: running additional tests has shown some problems
    ...fixed those
    
    Signed-off-by: Sven Oliver Moll <svolli@svolli.de>

--- src/tools/local-exomizer.sh
+++ src/tools/local-exomizer.sh
@@ -8,6 +8,7 @@ readonly URL="https://bitbucket.org/magli143/exomizer/wiki/downloads/${PACKAGE}.
 
 # setup build environment
 [ -f "$(basename "${URL}")" ] || wget "${URL}"
+rm -rf "${PACKAGE}"
 mkdir "${PACKAGE}"
 cd "${PACKAGE}"
 7z x "../$(basename "${URL}")"
@@ -19,7 +20,7 @@ make -C src -j${JOBS}
 ${sudo_package} install -s -m 0755 -D src/exomizer "${STOW_DIR}/${PACKAGE}/bin/exomizer"
 ${sudo_package} install -s -m 0755 -D src/exobasic "${STOW_DIR}/${PACKAGE}/bin/exobasic"
 
-for file in *.txt $(find exodecrs rawdecrs); do
+for file in *.txt $(find exodecrs rawdecrs -type f); do
    ${sudo_package} install "${file}" -m 0644 -D "${STOW_DIR}/${PACKAGE}/share/doc/${PACKAGE}/${file}"
 done
 
